6783072bba62e8690dc223fb752611b02896b414
ckacldk <36620916+ckacldk@users.noreply.github.com>
2026-01-24 09:28:38 +0000

    updated folder

--- week_1_docker_postgres_terraform/ingest_data.py
+++ week_1_docker_postgres_terraform/ingest_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+
 
 dtype = {
     "VendorID": "Int64",
@@ -25,20 +27,18 @@ parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
-  
-##df.head(0).to_sql(name='yellow_taxi_data', con=engine,if_exists='replace')
 
-def run():
-    year = 2021
-    month = 1
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    chunksize = 100000
-    target_table = 'yello_taxi_data'
-
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Table year')
+@click.option('--month', default=1, type=int, help='Table month')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='chunk size')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz' 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
@@ -64,7 +64,7 @@ def run():
             name=target_table, 
             con=engine, 
             if_exists='append'
-            )
+        )
     
 if __name__ == '__main__':
     run()
